d5572ba1ad7155b10cefae195a47ddf490901760
UEvstratov580 <uevstratov580@gmail.com>
2026-06-18 17:41:27 +0300

    Simplify main.cpp for Android build

--- main.cpp
+++ main.cpp
@@ -1,12 +1,16 @@
 #include <QApplication>
 #include <QPushButton>
+#include <QMessageBox>
 
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
     
-    QPushButton button("TapNovaPay Wallet");
+    QPushButton button("TapNovaPay");
     button.resize(300, 100);
+    QObject::connect(&button, &QPushButton::clicked, []() {
+        QMessageBox::information(nullptr, "Info", "TapNovaPay Wallet");
+    });
     button.show();
     
     return app.exec();
